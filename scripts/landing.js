44f7bc4f5e6725d59f6cf25a0888901c5df17d5e
Judy Wang <missjudywang@gmail.com>
2017-11-13 04:22:11 -0800

    Assignment 22 dom events completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,24 +1,21 @@
 var pointsArray = document.getElementsByClassName('point');
 
+var revealPoint = function(point) {
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleXtranslateY(0)";
+    point.style.WebkitTransform = "scaleXtranslateY(0)";
+  };
+
 var animatePoints = function(points) {
-
-   var revealPoint = function(index) {
-       points[index].style.opacity = 1;
-       points[index].style.transform = "scaleX(1) translateY(0)";
-       points[index].style.msTransform = "scaleXtranslateY(0)";
-       points[index].style.WebkitTransform = "scaleXtranslateY(0)";
-     }
-
-   for (var i=0; i<points.length; i++){
-      revealPoint(i);
-   }
+   forEach(points,revealPoint);
 };
 
 window.onload = function() {
    if (window.innerHeight > 950) {
         animatePoints(pointsArray);
    }
-   
+
    var sellingPoints = document.getElementsByClassName('selling-points')[0];
    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 
