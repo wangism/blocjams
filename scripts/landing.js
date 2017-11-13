8572519da21c158ea2141d4456f907805c1a39c1
Judy Wang <missjudywang@gmail.com>
2017-11-13 02:47:30 -0800

    Checkpoint 22 dom events completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,29 +1,31 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
 
-   var points = document.getElementsByClassName('point');
+var animatePoints = function(points) {
 
-   var revealFirstPoint = function() {
-      points[0].style.opacity = 1;
-      points[0].style.transform = "scaleX(1) translateY(0)";
-      points[0].style.msTransform = "scaleXtranslateY(0)";
-      points[0].style.WebkitTransform = "scaleXtranslateY(0)";
-   };
+   var revealPoint = function(index) {
+       points[index].style.opacity = 1;
+       points[index].style.transform = "scaleX(1) translateY(0)";
+       points[index].style.msTransform = "scaleXtranslateY(0)";
+       points[index].style.WebkitTransform = "scaleXtranslateY(0)";
+     }
 
-   var revealSecondPoint = function() {
-      points[1].style.opacity = 1;
-      points[1].style.transform = "scaleX(1) translateY(0)";
-      points[1].style.msTransform = "scaleXtranslateY(0)";
-      points[1].style.WebkitTransform = "scaleXtranslateY(0)";
-   };
-
-  var revealThirdPoint = function() {
-      points[2].style.opacity = 1;
-      points[2].style.transform = "scaleX(1) translateY(0)";
-      points[2].style.msTransform = "scaleXtranslateY(0)";
-      points[2].style.WebkitTransform = "scaleXtranslateY(0)";
-   };
-
-   revealFirstPoint();
-   revealSecondPoint();
-   revealThirdPoint();
+   for (var i=0; i<points.length; i++){
+      revealPoint(i);
+   }
 };
+
+window.onload = function() {
+   if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+   }
+   
+   var sellingPoints = document.getElementsByClassName('selling-points')[0];
+   var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+   window.addEventListener('scroll', function(event) {
+     if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);
+         }
+
+   });
+}
