9a646454fac1765ea45b0189830d872f9c0d112d
Judy Wang <missjudywang@gmail.com>
2017-11-22 21:55:16 -0800

    Assignment 24 dom album view completed

--- scripts/album.js
+++ scripts/album.js
@@ -56,15 +56,13 @@ var albumPicasso = {
      return template;
  };
 
- var setCurrentAlbum = function(album) {
-     // #1
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
-     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-     var albumImage = document.getElementsByClassName('album-cover-art')[0];
-     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+ var albumTitle = document.getElementsByClassName('album-view-title')[0];
+ var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+ var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+ var albumImage = document.getElementsByClassName('album-cover-art')[0];
+ var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
-     // #2
+ var setCurrentAlbum = function(album) {
      albumTitle.firstChild.nodeValue = album.name;
      albumArtist.firstChild.nodeValue = album.artist;
      albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
@@ -81,4 +79,15 @@ var albumPicasso = {
 
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
+
+     var albums = [albumPicasso, albumMarconi, albumSwift];
+     var index = 1;
+
+     albumImage.addEventListener("click", function(event){
+       setCurrentAlbum(albums[index]);
+       index++;
+       if (index == albums.length){
+         index = 0;
+       }
+     });
  };
