5f4a402ca783a954415ef23ed16f7f3abd72ab05
Judy Wang <missjudywang@gmail.com>
2018-02-01 08:46:16 -0800

    Checkpoint 33 jquery buzz library completed

--- scripts/album.js
+++ scripts/album.js
@@ -1,6 +1,23 @@
 var setSong = function(songNumber) {
+    if (currentSoundFile) {
+      currentSoundFile.stop();
+    }
+
     currentlyPlayingSongNumber = parseInt(songNumber);
     currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+
+    currentSoundFile = new buzz.sound(currentSongFromAlbum.audioUrl, {
+      formats: ["mp3"],
+      preload: true
+    });
+
+    setVolume(currentVolume);
+};
+
+var setVolume = function(volume) {
+    if (currentSoundFile) {
+       currentSoundFile.setVolume(volume);
+    }
 };
 
 var getSongNumberCell = function(number) {
@@ -28,15 +45,22 @@ var createSongRow = function(songNumber, songName, songLength) {
       };
       if (currentlyPlayingSongNumber !== songNumber) {
       // Switch from Play -> Pause button to indicate new song is playing.
-         $(this).html(pauseButtonTemplate);
          setSong(songNumber);
+         currentSoundFile.play();
+         $(this).html(pauseButtonTemplate);
+         currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
          updatePlayerBarSong();
       } else if (currentlyPlayingSongNumber === songNumber) {
        // Switch from Pause -> Play button to pause currently playing song.
-         $(this).html(playButtonTemplate);
-         $(".main-controls .play-pause").html(playerBarPlayButton);
-         currentlyPlayingSongNumber = null;
-         currentSongFromAlbum = null;
+           if (currentSoundFile.isPaused()) {
+             $(this).html(pauseButtonTemplate);
+             $('.main-controls .play-pause').html(playerBarPauseButton);
+             currentSoundFile.play();
+           } else {
+             $(this).html(playButtonTemplate);
+             $('.main-controls .play-pause').html(playerBarPlayButton);
+             currentSoundFile.pause();
+           }
       }
  };
 
@@ -99,8 +123,9 @@ var offHover = function(event) {
    var lastSongNumber = parseInt(currentlyPlayingSongNumber);
 
    // Set a new current song
-   currentlyPlayingSongNumber = currentSongIndex + 1;
+   setSong(currentSongIndex + 1);
    currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
+   currentSoundFile.play();
 
    // Update the Player Bar info
    updatePlayerBarSong();
@@ -130,8 +155,9 @@ var offHover = function(event) {
    var lastSongNumber = parseInt(currentlyPlayingSongNumber);
 
    // Set a new current song
-   currentlyPlayingSongNumber = currentSongIndex + 1;
+   setSong(currentSongIndex + 1);
    currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
+   currentSoundFile.play();
 
    // Update the Player Bar info
    updatePlayerBarSong();
@@ -164,8 +190,9 @@ var playButtonTemplate = '<span class="ion-play"></span>';
 var pauseButtonTemplate = '<span class="ion-pause"></span>';
 
 var currentAlbum = null;
-var currentlyPlayingSongNumber = parseInt(null);
+var currentlyPlayingSongNumber = null;
 var currentSongFromAlbum = null;
+var currentSoundFile = null;
 
 var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next');
